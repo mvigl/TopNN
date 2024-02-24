1c52d89978ebdc1f6ebcb1e5752be81117e059cf
mavigl <matthias.vigl@ern.ch>
2024-02-24 11:12:47 +0100

    Vrtual links

--- maps.py
+++ maps.py
@@ -20,9 +20,9 @@ if __name__ == "__main__":
             for line in f:
                 filename = line.strip()
                 data_index = (filename.index("/mc"))
-                path = (filename[:data_index])
                 name = (filename[data_index+1:])
                 print(name)
+                if 'MCRun2_Signal_AF3' in filename: name = 'MCRun2_Signal_AF3_' + name 
                 h5fw[name] = h5py.ExternalLink(filename,'/')   
 
 
