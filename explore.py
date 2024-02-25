eec704ff72808130d0b44f6c5981e377b65d6fe2
mavigl <matthias.vigl@ern.ch>
2024-02-25 02:04:29 +0100

    Valid

--- explore.py
+++ explore.py
@@ -16,8 +16,12 @@ with open(filelist) as f:
         with h5py.File(filename, 'r') as f:
             labels = f['labels'][:]
             odd_labels = np.sum(labels==0.5)
+            pos_labels = np.sum(labels==1)
+            neg_labels = np.sum(labels==0)
             all_labels = len(labels)
             print('odd labels: ', odd_labels, ' out of: ', all_labels, ' meaning: ',(odd_labels/all_labels)*100, 'percent' )
+            print('pos labels: ', pos_labels, ' out of: ', all_labels, ' meaning: ',(pos_labels/all_labels)*100, 'percent' )
+            print('neg labels: ', neg_labels, ' out of: ', all_labels, ' meaning: ',(neg_labels/all_labels)*100, 'percent' )
 
 
     
