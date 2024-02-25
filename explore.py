6f2f1b1b0e1c8fd78ecb853fa9ce46323bc223f4
mavigl <matthias.vigl@ern.ch>
2024-02-25 01:58:25 +0100

    Valid

---
+++ explore.py
@@ -0,0 +1,23 @@
+import numpy as np
+import uproot
+import awkward as ak
+import argparse
+import pickle
+import h5py
+import os
+
+
+
+filelist = '/raven/u/mvigl/Stop/TopNN/data/H5/list_all.txt'
+with open(filelist) as f:
+    for line in f:
+        filename = line.strip()
+        print('reading : ',filename)
+        with h5py.File(filename, 'r') as f:
+            labels = f['labels'][:]
+            odd_labels = np.sum(labels==0.5)
+            all_labels = len(labels)
+            print('odd labels: ', odd_labels, ' out of: ', all_labels, ' meaning: ',(odd_labels/all_labels)*100, 'percent' )
+
+
+    
