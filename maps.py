98fcc1446533b91d17f01c8cad0d845cd288b4b1
mavigl <matthias.vigl@ern.ch>
2024-02-29 01:04:04 +0100

    plots

--- maps.py
+++ maps.py
@@ -14,8 +14,8 @@ import h5py
   
 
 if __name__ == "__main__":
-    filelist = '/raven/u/mvigl/Stop/TopNN/data/H5/list_all.txt'
-    with h5py.File('/raven/u/mvigl/Stop/data/H5_full/Virtual_full.h5',mode='w') as h5fw:
+    filelist = '/raven/u/mvigl/Stop/TopNN/data/H5/test/list_all.txt'
+    with h5py.File('/raven/u/mvigl/Stop/data/H5_full/Virtual_test.h5',mode='w') as h5fw:
         with open(filelist) as f:
             for line in f:
                 filename = line.strip()
