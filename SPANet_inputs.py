1f22f2a42b4b6a3f7268e8ba5a2bb55d9d92d583
mavigl <matthias.vigl@ern.ch>
2024-04-21 19:16:49 +0200

    spanet ete inputs

--- SPANet_inputs.py
+++ SPANet_inputs.py
@@ -8,8 +8,8 @@ import os
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--filelist', help='data',default='train_list_testing.txt')
-parser.add_argument('--split', help='train,test,val',default='test')
-parser.add_argument('--out_dir', help='out_dir',default='H5_ete_spanet_stop_FS')
+parser.add_argument('--split', help='train,test,val',default='train')
+parser.add_argument('--out_dir', help='out_dir',default='H5_ete_spanet_stop_all_small')
 args = parser.parse_args()
 
 def split_data(length,array,dataset='test'):
