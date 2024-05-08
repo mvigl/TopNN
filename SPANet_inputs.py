9a95d4f3031923edd97c076ff011af555a6dfad0
mavigl <matthias.vigl@ern.ch>
2024-05-08 13:31:42 +0200

    masses

--- SPANet_inputs.py
+++ SPANet_inputs.py
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser(description='')
 parser.add_argument('--filelist', help='data',default='data/root/list_sig_FS_testing.txt')
 parser.add_argument('--split', help='train,test,val',default='train')
 parser.add_argument('--out_dir', help='out_dir',default='SPANet_all_8_cat')
+parser.add_argument('--combine',  action='store_true', help='combine', default=False)
 args = parser.parse_args()
 
 def split_data(length,array,dataset='test'):
@@ -293,118 +294,166 @@ variables = ['truth_top_min_dR',
             'EventNumber',
             ]
 
+def save_combined(args):
+        with open(args.filelist) as f:
+            i=0
+            for line in f:
+                filename = line.strip()
+                print('reading : ',filename)
+                sig=False
+                if '_Signal_' in filename: sig=True
+                with uproot.open({filename: "stop1L_NONE;1"}) as tree:
+                    number = number = filename[(filename.index("TeV.")+4):(filename.index(".stop1L"))]
+                    branches = tree.arrays(Features)
+                    mask_i,inputs_i,targets_i,out_truth_info_i,met_i,signal_i = get_data(branches,dataset=dataset,sig=sig,number=number)
+                    if i==0:
+                        mask = mask_i
+                        inputs = inputs_i
+                        targets = targets_i
+                        out_truth_info = out_truth_info_i
+                        met = met_i
+                        signal = signal_i
+                    else:
+                        mask = np.concatenate((mask,mask_i),axis=0)
+                        inputs = merge(inputs,inputs_i)
+                        targets = merge(targets,targets_i)
+                        out_truth_info = merge(out_truth_info,out_truth_info_i)
+                        met = merge(met,met_i)
+                        signal = np.concatenate((signal,signal_i),axis=0)
+                    i+=1
+                out_dir = f'{args.out_dir}/'
+                if (not os.path.exists(out_dir)): os.system(f'mkdir {out_dir}')
+                out_f = out_dir + f'/spanet_inputs_{dataset}.h5'
+                with h5py.File(out_f, 'w') as out_file: 
+                    classifications_group = out_file.create_group('CLASSIFICATIONS')
+                    event = classifications_group.create_group(f'EVENT')
+                    event.create_dataset('signal', data=signal, dtype='int64')
+                    match = np.maximum(out_truth_info['truth_topp_match'],out_truth_info['truth_topm_match'])
+                    match += 2
+                    only_sig_match = False
+                    if only_sig_match:
+                        if sig == False:
+                            match[match!=0] = -1
+                    event.create_dataset('match', data=match,dtype='int64')
+
+                    inputs_group = out_file.create_group('INPUTS')
+                    Momenta = inputs_group.create_group(f'Momenta')
+                    Momenta.create_dataset('MASK', data=mask, dtype='bool')
+                    Momenta.create_dataset('btag', data=inputs['btag'])
+                    Momenta.create_dataset('qtag', data=inputs['qtag'])
+                    Momenta.create_dataset('etag', data=inputs['etag'])
+                    Momenta.create_dataset('eta', data=inputs['eta'])
+                    Momenta.create_dataset('mass', data=inputs['M'])
+                    Momenta.create_dataset('phi', data=inputs['phi'])
+                    Momenta.create_dataset('pt', data=inputs['pT'])
+
+                    Met = inputs_group.create_group(f'Met')  
+                    Met.create_dataset('MET', data=met['MET'],dtype='float32')   
+                    Met.create_dataset('METsig', data=met['METsig'],dtype='float32')
+                    Met.create_dataset('METphi', data=met['METphi'],dtype='float32')
+                    Met.create_dataset('MET_Soft', data=met['MET_Soft'],dtype='float32')
+                    Met.create_dataset('MET_Jet', data=met['MET_Jet'],dtype='float32')
+                    Met.create_dataset('MET_Ele', data=met['MET_Ele'],dtype='float32')
+                    Met.create_dataset('MET_Muon', data=met['MET_Muon'],dtype='float32')
+                    Met.create_dataset('mT_METl', data=met['mT_METl'],dtype='float32')
+                    Met.create_dataset('dR_bb', data=met['dR_bb'],dtype='float32')
+                    Met.create_dataset('dphi_METl', data=met['dphi_METl'],dtype='float32')
+                    Met.create_dataset('MT2_bb', data=met['MT2_bb'],dtype='float32')
+                    Met.create_dataset('MT2_b1l1_b2', data=met['MT2_b1l1_b2'],dtype='float32')
+                    Met.create_dataset('MT2_b2l1_b1', data=met['MT2_b2l1_b1'],dtype='float32')
+                    Met.create_dataset('MT2_min', data=met['MT2_min'],dtype='float32') 
+
+                    targets_group = out_file.create_group('TARGETS')
+                    ht = targets_group.create_group(f'ht')
+                    ht.create_dataset('b', data=targets['htb'],dtype='int64')
+                    ht.create_dataset('q1', data=targets['q1'],dtype='int64')
+                    ht.create_dataset('q2', data=targets['q2'],dtype='int64')
+                    lt = targets_group.create_group(f'lt')
+                    lt.create_dataset('b', data=targets['ltb'],dtype='int64')
+                    lt.create_dataset('l', data=targets['ltl'],dtype='int64')
+
+                    regressions_group = out_file.create_group('REGRESSIONS')
+                    regression = regressions_group.create_group(f'EVENT')
+
+                    truth_info_group = out_file.create_group('truth_info')
+                    for info in out_truth_info.keys():
+                        truth_info_group.create_dataset(info, data=out_truth_info[info])
+
+def save_single(args):
+        with open(args.filelist) as f:
+            for line in f:
+                filename = line.strip()
+                print('reading : ',filename)
+                sig=False
+                if '_Signal_' in filename: sig=True
+                with uproot.open({filename: "stop1L_NONE;1"}) as tree:
+                    number = number = filename[(filename.index("TeV.")+4):(filename.index(".stop1L"))]
+                    sub_dir = filename[(filename.index("/MC")):(filename.index("/mc2"))]
+                    out_f = filename[(filename.index("/mc2")):].replace(".root",f"_{dataset}.h5")
+                    branches = tree.arrays(Features)
+                    mask,inputs,targets,out_truth_info,met,signal = get_data(branches,dataset=dataset,sig=sig,number=number)
+                    out_dir = f'{args.out_dir}/'
+                    if (not os.path.exists(out_dir)): os.system(f'mkdir {out_dir}')
+                    sub_dir = f'{out_dir}{sub_dir}'
+                    if (not os.path.exists(sub_dir)): os.system(f'mkdir {sub_dir}')
+                    out_f = f'{out_dir}{sub_dir}{out_f}'
+                    with h5py.File(out_f, 'w') as out_file: 
+                        classifications_group = out_file.create_group('CLASSIFICATIONS')
+                        event = classifications_group.create_group(f'EVENT')
+                        event.create_dataset('signal', data=signal, dtype='int64')
+                        match = np.maximum(out_truth_info['truth_topp_match'],out_truth_info['truth_topm_match'])
+                        match += 2
+                        only_sig_match = False
+                        if only_sig_match:
+                            if sig == False:
+                                match[match!=0] = -1
+                        event.create_dataset('match', data=match,dtype='int64')
+
+                        inputs_group = out_file.create_group('INPUTS')
+                        Momenta = inputs_group.create_group(f'Momenta')
+                        Momenta.create_dataset('MASK', data=mask, dtype='bool')
+                        Momenta.create_dataset('btag', data=inputs['btag'])
+                        Momenta.create_dataset('qtag', data=inputs['qtag'])
+                        Momenta.create_dataset('etag', data=inputs['etag'])
+                        Momenta.create_dataset('eta', data=inputs['eta'])
+                        Momenta.create_dataset('mass', data=inputs['M'])
+                        Momenta.create_dataset('phi', data=inputs['phi'])
+                        Momenta.create_dataset('pt', data=inputs['pT'])
+
+                        Met = inputs_group.create_group(f'Met')  
+                        Met.create_dataset('MET', data=met['MET'],dtype='float32')   
+                        Met.create_dataset('METsig', data=met['METsig'],dtype='float32')
+                        Met.create_dataset('METphi', data=met['METphi'],dtype='float32')
+                        Met.create_dataset('MET_Soft', data=met['MET_Soft'],dtype='float32')
+                        Met.create_dataset('MET_Jet', data=met['MET_Jet'],dtype='float32')
+                        Met.create_dataset('MET_Ele', data=met['MET_Ele'],dtype='float32')
+                        Met.create_dataset('MET_Muon', data=met['MET_Muon'],dtype='float32')
+                        Met.create_dataset('mT_METl', data=met['mT_METl'],dtype='float32')
+                        Met.create_dataset('dR_bb', data=met['dR_bb'],dtype='float32')
+                        Met.create_dataset('dphi_METl', data=met['dphi_METl'],dtype='float32')
+                        Met.create_dataset('MT2_bb', data=met['MT2_bb'],dtype='float32')
+                        Met.create_dataset('MT2_b1l1_b2', data=met['MT2_b1l1_b2'],dtype='float32')
+                        Met.create_dataset('MT2_b2l1_b1', data=met['MT2_b2l1_b1'],dtype='float32')
+                        Met.create_dataset('MT2_min', data=met['MT2_min'],dtype='float32') 
+
+                        targets_group = out_file.create_group('TARGETS')
+                        ht = targets_group.create_group(f'ht')
+                        ht.create_dataset('b', data=targets['htb'],dtype='int64')
+                        ht.create_dataset('q1', data=targets['q1'],dtype='int64')
+                        ht.create_dataset('q2', data=targets['q2'],dtype='int64')
+                        lt = targets_group.create_group(f'lt')
+                        lt.create_dataset('b', data=targets['ltb'],dtype='int64')
+                        lt.create_dataset('l', data=targets['ltl'],dtype='int64')
+
+                        regressions_group = out_file.create_group('REGRESSIONS')
+                        regression = regressions_group.create_group(f'EVENT')
+
+                        truth_info_group = out_file.create_group('truth_info')
+                        for info in out_truth_info.keys():
+                            truth_info_group.create_dataset(info, data=out_truth_info[info])                        
+                        
 if __name__ == '__main__':
     dataset = args.split
-    with open(args.filelist) as f:
-        i=0
-        for line in f:
-            filename = line.strip()
-            print('reading : ',filename)
-            sig=False
-            if '_Signal_' in filename: sig=True
-            with uproot.open({filename: "stop1L_NONE;1"}) as tree:
-                number = number = filename[(filename.index("TeV.")+4):(filename.index(".stop1L"))]
-                branches = tree.arrays(Features)
-                mask_i,inputs_i,targets_i,out_truth_info_i,met_i,signal_i = get_data(branches,dataset=dataset,sig=sig,number=number)
-                if i==0:
-                    mask = mask_i
-                    inputs = inputs_i
-                    targets = targets_i
-                    out_truth_info = out_truth_info_i
-                    met = met_i
-                    signal = signal_i
-                else:
-                    mask = np.concatenate((mask,mask_i),axis=0)
-                    inputs = merge(inputs,inputs_i)
-                    targets = merge(targets,targets_i)
-                    out_truth_info = merge(out_truth_info,out_truth_info_i)
-                    met = merge(met,met_i)
-                    signal = np.concatenate((signal,signal_i),axis=0)
-                i+=1
-            out_dir = f'{args.out_dir}/'
-            if (not os.path.exists(out_dir)): os.system(f'mkdir {out_dir}')
-            out_f = out_dir + f'/spanet_inputs_{dataset}.h5'
-            with h5py.File(out_f, 'w') as out_file: 
-                classifications_group = out_file.create_group('CLASSIFICATIONS')
-                event = classifications_group.create_group(f'EVENT')
-                event.create_dataset('signal', data=signal, dtype='int64')
-                match = np.maximum(out_truth_info['truth_topp_match'],out_truth_info['truth_topm_match'])
-                match += 2
-                only_sig_match = False
-                if only_sig_match:
-                    if sig == False:
-                        match[match!=0] = -1
-                event.create_dataset('match', data=match,dtype='int64')
-
-                inputs_group = out_file.create_group('INPUTS')
-                Momenta = inputs_group.create_group(f'Momenta')
-                Momenta.create_dataset('MASK', data=mask, dtype='bool')
-                Momenta.create_dataset('btag', data=inputs['btag'])
-                Momenta.create_dataset('qtag', data=inputs['qtag'])
-                Momenta.create_dataset('etag', data=inputs['etag'])
-                Momenta.create_dataset('eta', data=inputs['eta'])
-                Momenta.create_dataset('mass', data=inputs['M'])
-                Momenta.create_dataset('phi', data=inputs['phi'])
-                Momenta.create_dataset('pt', data=inputs['pT'])
-               
-                Met = inputs_group.create_group(f'Met')  
-                Met.create_dataset('MET', data=met['MET'],dtype='float32')   
-                Met.create_dataset('METsig', data=met['METsig'],dtype='float32')
-                Met.create_dataset('METphi', data=met['METphi'],dtype='float32')
-                Met.create_dataset('MET_Soft', data=met['MET_Soft'],dtype='float32')
-                Met.create_dataset('MET_Jet', data=met['MET_Jet'],dtype='float32')
-                Met.create_dataset('MET_Ele', data=met['MET_Ele'],dtype='float32')
-                Met.create_dataset('MET_Muon', data=met['MET_Muon'],dtype='float32')
-                Met.create_dataset('mT_METl', data=met['mT_METl'],dtype='float32')
-                Met.create_dataset('dR_bb', data=met['dR_bb'],dtype='float32')
-                Met.create_dataset('dphi_METl', data=met['dphi_METl'],dtype='float32')
-                Met.create_dataset('MT2_bb', data=met['MT2_bb'],dtype='float32')
-                Met.create_dataset('MT2_b1l1_b2', data=met['MT2_b1l1_b2'],dtype='float32')
-                Met.create_dataset('MT2_b2l1_b1', data=met['MT2_b2l1_b1'],dtype='float32')
-                Met.create_dataset('MT2_min', data=met['MT2_min'],dtype='float32') 
-                
-                targets_group = out_file.create_group('TARGETS')
-                ht = targets_group.create_group(f'ht')
-                ht.create_dataset('b', data=targets['htb'],dtype='int64')
-                ht.create_dataset('q1', data=targets['q1'],dtype='int64')
-                ht.create_dataset('q2', data=targets['q2'],dtype='int64')
-                lt = targets_group.create_group(f'lt')
-                lt.create_dataset('b', data=targets['ltb'],dtype='int64')
-                lt.create_dataset('l', data=targets['ltl'],dtype='int64')
-
-                regressions_group = out_file.create_group('REGRESSIONS')
-                regression = regressions_group.create_group(f'EVENT')
-
-                truth_info_group = out_file.create_group('truth_info')
-                for info in out_truth_info.keys():
-                    truth_info_group.create_dataset(info, data=out_truth_info[info])
-
-    #with open(args.filelist) as f:
-    #    for line in f:
-    #        filename = line.strip()
-    #        print('reading : ',filename)
-    #        with uproot.open({filename: "stop1L_NONE;1"}) as tree:
-    #            out_dir = f'{args.out_dir}_{dataset}/'
-    #            branches = tree.arrays(Features)
-    #            mask,inputs,targets,out_truth_info = get_data(branches,dataset=dataset)
-    #            if (not os.path.exists(out_dir)): os.system(f'mkdir {out_dir}')
-    #            data_index = filename.index("/MC")
-    #            out_dir = out_dir + (filename[data_index:]).replace(".root",f"_{dataset}.h5")
-    #            out_index = out_dir.index("/mc2") 
-    #            if (not os.path.exists(out_dir[:out_index])): os.system(f'mkdir {out_dir[:out_index]}')
-    #            with h5py.File(out_dir, 'w') as out_file: 
-    #                inputs_group = out_file.create_group('INPUTS')
-    #                source = inputs_group.create_group(f'Source')
-    #                source.create_dataset('MASK', data=mask)
-    #                source.create_dataset('btag', data=inputs['btag'])
-    #                source.create_dataset('eta', data=inputs['eta'])
-    #                source.create_dataset('mass', data=inputs['M'])
-    #                source.create_dataset('phi', data=inputs['phi'])
-    #                source.create_dataset('pt', data=inputs['pT'])
-    #                targets_group = out_file.create_group('TARGETS')
-    #                ht = targets_group.create_group(f'ht')
-    #                ht.create_dataset('b', data=targets['htb'],dtype='i4')
-    #                ht.create_dataset('q1', data=targets['q1'],dtype='i4')
-    #                ht.create_dataset('q2', data=targets['q2'],dtype='i4')
-    #                lt = targets_group.create_group(f'lt')
-    #                lt.create_dataset('b', data=targets['ltb'],dtype='i4')
+    if args.combine : save_combined(args)
+    else: save_single(args)
+        
